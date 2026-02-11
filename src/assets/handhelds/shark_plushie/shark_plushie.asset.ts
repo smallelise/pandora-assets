8bfaf080df0a1235f2b10ea3f510728641bafc57
smallelise <elise21dev@proton.me>
2026-02-11 23:10:01 +0100

    indev prototype

---
+++ src/assets/handhelds/shark_plushie/shark_plushie.asset.ts
@@ -0,0 +1,60 @@
+DefineAsset({
+	name: 'Shark Plushie',
+	size: 'medium',
+	graphics: 'graphics.json',
+	colorization: {
+		belly: {
+			name: 'Belly',
+			default: '#F0F3F5',
+		},
+		body: {
+			name: 'Body',
+			default: '#567890',
+		},
+		pupil: {
+			name: 'Pupil',
+			default: '#25252d',
+		},
+		iris: {
+			name: 'Iris',
+			default: '#BF5249',
+		},
+		sciera: {
+			name: 'Sciera',
+			default: '#e6e7ea',
+		},
+		cavity: {
+			name: 'Cavity',
+			default: '#873f39',
+		},
+		teeth: {
+			name: 'Teeth',
+			default: '#e1e3e5',
+		},
+	},
+	//roomDeployment: {
+	//	autoDeployRelativePosition: [330, -150, 0],
+	//},
+	preview: undefined,
+	ownership:
+        {
+        	responsibleContributor: 'smallelise <elise21dev@proton.me>',
+        	credits:
+                ['smallelise'],
+        	modificationPolicy:
+                'Fixes only, otherwise ask',
+        	reusePolicy:
+                'Free to use',
+        	licensing:
+                [
+                	{
+                		part: 'Blahaj',
+                		source: 'Self-Made',
+                		copyrightHolder: 'smallelise',
+                		editedBy: 'smallelise',
+                		license: 'CC BY-SA',
+                	},
+                ],
+        },
+});
+
